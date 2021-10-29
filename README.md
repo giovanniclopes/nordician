6c08028ab89428d5b61dba2564c02f526495cd67
giovanniclopes <giovanniclopes@gmail.com>
2021-10-28 22:28:36 -0300

    att

---
+++ README.md
@@ -0,0 +1,6 @@
+# WIKI-VIKING
+#### :shield: :bow_and_arrow: *_https://giovanniclopes.github.io/wiki-viking_* :crossed_swords: :axe:
+> Um site wiki que traz informações de toda a cultura nórdica. Aproveite!
+
+## Do que se trata o site?
+ :speech_balloon: Olá viajante, bem vinda(o) ao site Wiki Viking. Aqui fornecemos diversos artigos para quem busca por informações sobre a cultura nórdica. Contendo artigos sobre o mundo real, e sobre o mundo místico.
---
+++ script.js
@@ -0,0 +1,28 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+  }
+
+  function myFunction2() {
+    document.getElementById("myDropdown2").classList.toggle("show2");
+    if (myFunction() == show) {
+        removeEventListener('show2')
+    }
+  }
+  
+  window.onclick2 = function(e) {
+    if (!e.target.matches('.dropbtn2')) {
+    var myDropdown2 = document.getElementById("myDropdown2");
+      if (myDropdown2.classList.contains('show2')) {
+        myDropdown2.classList.remove('show2');
+      }
+    }
+  }
